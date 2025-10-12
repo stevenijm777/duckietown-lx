6b0bb0559d4febe34fa9e403d32fb064d256b968
stevenijm777 <stemjara@espol.edu.ec>
2025-10-12 13:47:46 -0500

    Implementé las funciones de matriz para los motores izquierdo y derecho

--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-lower_hsv = np.array([171, 140, 100])
-upper_hsv = np.array([179, 200, 255])
+lower_hsv = np.array([20, 153, 153])
+upper_hsv = np.array([40, 255, 255])
 
 
 def preprocess(image_rgb: np.ndarray) -> np.ndarray:
