d53a61162e4c56492152a73adbd9e64fe8314258
stevenijm777 <yosoystevenijm@gmail.com>
2025-11-16 11:11:44 -0500

    solution 3 - braintenberg

--- braitenberg/packages/braitenberg_agent/agent.py
+++ braitenberg/packages/braitenberg_agent/agent.py
@@ -25,7 +25,7 @@ from solution.preprocessing import preprocess
 # TODO edit this Config class ! Play with different gain and const values
 @dataclass
 class BraitenbergAgentConfig:
-    gain: float = 0.5
+    gain: float = 0.3
     const: float = 0.3
 
 
